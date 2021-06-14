3b7bd2eaf78e78746e99595d85b5df3bc8cb3245
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2021-06-14 12:16:15 +0530

    Modified the time scale to reduce the hardness of the gameplay.
    - Initial time scale is set to 0.8.
    - `speedup_factor` is set to 0.0002.
    - For every frame, the time scale is updated linearly based on the speedup factor and delta time.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,22 @@ public class PlayerMovement : MonoBehaviour
     float forwardForce = 2000f;
     float sidewardForce = 100f;
     static float sensitivity = 0.6f;
-    float speedup_factor = 0.00005f;
+    float speedup_factor = 0.0002f;
     // Use this for initialization
     void Start()
     {
         rb = GameObject.Find("player").GetComponent<Rigidbody>();
         setSensitivity(PlayerPrefs.GetFloat("Sensitivity", 0.6f));
+        // Set initial time scale to 0.8
+        Time.timeScale = 0.8f;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!PauseMenu.isGamePaused() && Time.timeScale < 2)
-            Time.timeScale += speedup_factor;
+            Time.timeScale += speedup_factor * Time.deltaTime;
+        Debug.Log("TimeScale: " + Time.timeScale);
     }
 
     void FixedUpdate()
